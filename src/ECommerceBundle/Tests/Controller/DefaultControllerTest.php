ad043428f2ca7b85b2653f103a24645d08e960f9
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-22 20:47:37 -0400

    Added contoller test to the ecommerce

---
+++ src/ECommerceBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ECommerceBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
