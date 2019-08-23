6d16e8e6c7bbaa363938aa96b2d4af23cbbb7035
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-23 14:11:29 -0400

    Added Gender Entity

---
+++ src/GruponivCPH/ServiceBundle/Entity/Gender.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace GruponivCPH\ServiceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Gender
+ *
+ * @ORM\Table(name="gender")
+ * @ORM\Entity
+ */
+class Gender
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+    
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sex", type="string", length=255)
+     */
+    private $sex;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Gender
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    public function __toString() {
+        return $this->name;
+    }
+
+    /**
+     * Set sex
+     *
+     * @param string $sex
+     * @return Gender
+     */
+    public function setSex($sex)
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+
+    /**
+     * Get sex
+     *
+     * @return string 
+     */
+    public function getSex()
+    {
+        return $this->sex;
+    }
+}
