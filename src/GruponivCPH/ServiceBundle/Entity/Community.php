37f8c6136f14f7f3710529ef6946822116244a9a
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-24 10:04:05 -0400

    Added Community entity

---
+++ src/GruponivCPH/ServiceBundle/Entity/Community.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace GruponivCPH\ServiceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Community
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Community
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Community
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    
+    public function __toString() {
+        return $this->name;
+    }
+}
