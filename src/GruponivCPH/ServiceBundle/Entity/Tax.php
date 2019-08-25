ed5ac8d38642b3e5d240281bddf9e62d13e4fc43
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-24 21:53:46 -0400

    Added Tax Entity

---
+++ src/GruponivCPH/ServiceBundle/Entity/Tax.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace GruponivCPH\ServiceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tax
+ *
+ * @ORM\Table(name="tax")
+ * @ORM\Entity
+ */
+class Tax
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     * @return Tax
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
