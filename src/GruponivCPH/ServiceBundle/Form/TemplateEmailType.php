b32284bd114ec308044bb8a0e3cd12023862e6f0
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-25 14:08:31 -0400

    Added TemplateEmailType Form

---
+++ src/GruponivCPH/ServiceBundle/Form/TemplateEmailType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace GruponivCPH\ServiceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TemplateEmailType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('activate')
+            ->add('subject')
+            ->add('recipient')
+            ->add('header')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GruponivCPH\ServiceBundle\Entity\TemplateEmail'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'gruponivcph_servicebundle_templateemail';
+    }
+}
