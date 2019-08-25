0624b1b544641ac29cad77cd2a6802ee5c0b8bc3
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-24 21:55:58 -0400

    Added LanguageType Form

---
+++ src/GruponivCPH/ServiceBundle/Form/LanguageType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace GruponivCPH\ServiceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class LanguageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('codeIso')
+            ->add('codeLanguage')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GruponivCPH\ServiceBundle\Entity\Language'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'gruponivcph_servicebundle_language';
+    }
+}
