7d5c7b93f962b569283b50150aa0e033ddb9222a
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-25 14:07:15 -0400

    Added ZoneType Form

---
+++ src/GruponivCPH/ServiceBundle/Form/ZoneType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace GruponivCPH\ServiceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ZoneType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('pricefree');
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GruponivCPH\ServiceBundle\Entity\ZoneSend'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'gruponivcph_servicebundle_zone';
+    }
+}
