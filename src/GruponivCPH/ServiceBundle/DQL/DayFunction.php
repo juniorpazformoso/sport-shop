7fdb736802468fe858b02ef82e1f9bdf91f41cae
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-25 09:18:06 -0400

    Added Extended DQL Function

---
+++ src/GruponivCPH/ServiceBundle/DQL/DayFunction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\DQL;
+
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\Parser;
+
+/**
+ * Class DateFormatFunction
+ *
+ * Adds the hability to use the MySQL DATE_FORMAT function inside Doctrine
+ *
+ * @package Vf\Bundle\VouchedforBundle\DQL
+ */
+class DayFunction extends FunctionNode
+{
+
+	/**
+	 * Holds the timestamp of the DATE_FORMAT DQL statement
+	 * @var $dateExpression
+	 */
+	protected $dateExpression;
+
+	/**
+	 * Holds the '% format' parameter of the DATE_FORMAT DQL statement
+	 * var String
+	 */
+	protected $formatChar;
+
+	public function getSql( SqlWalker $sqlWalker )
+	{
+		return 'DAY (' . $sqlWalker->walkArithmeticExpression( $this->dateExpression ) . ')';
+	}
+
+	public function parse( Parser $parser )
+	{
+            $parser->Match( Lexer::T_IDENTIFIER );
+            $parser->Match( Lexer::T_OPEN_PARENTHESIS );
+            $this->dateExpression = $parser->ArithmeticExpression();
+            $parser->Match( Lexer::T_CLOSE_PARENTHESIS );
+	}
+}
