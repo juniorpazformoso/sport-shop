48482c010e54a7c46709465c1e5f645ccbee88c4
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-26 18:26:43 -0400

    Added TechnologyType Form

---
+++ src/GruponivCPH/ServiceBundle/Form/TechnologyType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace GruponivCPH\ServiceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TechnologyType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('imageFile', 'file')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GruponivCPH\ServiceBundle\Entity\Technology'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'gruponivcph_servicebundle_technology';
+    }
+}
