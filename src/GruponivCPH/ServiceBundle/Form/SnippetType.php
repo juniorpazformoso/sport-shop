f5a0be3387f886cd6b5cc59a8869ee2cb8f67833
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-26 20:56:59 -0400

    Added SnippetType Form

---
+++ src/GruponivCPH/ServiceBundle/Form/SnippetType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace GruponivCPH\ServiceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SnippetType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content', 'textarea', array('required' => false))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GruponivCPH\ServiceBundle\Entity\Snippet'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'gruponivcph_servicebundle_snippet';
+    }
+}
