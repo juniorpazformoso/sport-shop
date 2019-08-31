a7ecee43e0b8bd0093d444db36cf6ede0273cf7f
Junior Paz Formoso <junior.presagios@gmail.com>
2019-08-31 12:39:07 -0400

    Added EmailSenderController

---
+++ src/GruponivCPH/ServiceBundle/Controller/EmailSenderController.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace GruponivCPH\ServiceBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use GruponivCPH\ServiceBundle\Entity\EmailSender;
+use GruponivCPH\ServiceBundle\Form\EmailSenderType;
+
+/**
+ * EmailSender controller.
+ *
+ */
+class EmailSenderController extends Controller
+{
+
+    /**
+     * Lists all EmailSender entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('ServiceBundle:EmailSender')->findAll();
+
+        return $this->render('ServiceBundle:EmailSender:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new EmailSender entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new EmailSender();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('emailsender_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('ServiceBundle:EmailSender:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a EmailSender entity.
+     *
+     * @param EmailSender $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(EmailSender $entity)
+    {
+        $form = $this->createForm(new EmailSenderType(), $entity, array(
+            'action' => $this->generateUrl('emailsender_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new EmailSender entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new EmailSender();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('ServiceBundle:EmailSender:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a EmailSender entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('ServiceBundle:EmailSender')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find EmailSender entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('ServiceBundle:EmailSender:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing EmailSender entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('ServiceBundle:EmailSender')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find EmailSender entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('ServiceBundle:EmailSender:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a EmailSender entity.
+    *
+    * @param EmailSender $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(EmailSender $entity)
+    {
+        $form = $this->createForm(new EmailSenderType(), $entity, array(
+            'action' => $this->generateUrl('emailsender_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing EmailSender entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('ServiceBundle:EmailSender')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find EmailSender entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('emailsender_edit', array('id' => $id)));
+        }
+
+        return $this->render('ServiceBundle:EmailSender:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a EmailSender entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('ServiceBundle:EmailSender')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find EmailSender entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('emailsender'));
+    }
+
+    /**
+     * Creates a form to delete a EmailSender entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('emailsender_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
