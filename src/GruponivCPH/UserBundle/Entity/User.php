a5ef21e1813dd56deef5f7065e2437726d556dbb
Junior Paz Formoso <junior.presagios@gmail.com>
2019-09-14 09:11:56 -0400

    Added User Entity

---
+++ src/GruponivCPH/UserBundle/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace GruponivCPH\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ * @UniqueEntity(fields="email", message="Una cuenta ya está registrada con este e-mail.")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
